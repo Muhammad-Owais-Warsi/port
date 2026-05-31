93fd34dfc08f7083d8803a20f50c5260c0d22452
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2026-05-31 17:52:50 +0100

    add support for math formula

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://owais.is-a.dev",
     base: "/",
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] }), sitemap(), react()],
     build: {
         assets: "assets",
     },
