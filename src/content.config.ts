ce997c02f91d67ca89d95ab78bf85255ff8cfa6b
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2026-09-09 11:35:32 +0100

    notes section

--- src/content.config.ts
+++ src/content.config.ts
@@ -30,4 +30,15 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const notes = defineCollection({
+	loader: glob({ base: './src/content/notes', pattern: '**/*.{md,mdx}' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			heroImage: z.optional(image()),
+		}),
+});
+
+export const collections = { blog, notes };
